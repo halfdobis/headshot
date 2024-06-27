eb22ae698083ffc5f64a3eaf56057d8ed0755940
halfdobis <missdobis@gmail.com>
2024-06-27 16:56:20 +0100

    fonts and landing layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import React from "react";
 import Landing from "@/components/landing/Landing";
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col p-6">
       <Landing />
     </main>
   );
