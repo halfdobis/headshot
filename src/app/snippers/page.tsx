9e5c6c4b34bf17eb0b7019b8ce97736c89930565
halfdobis <missdobis@gmail.com>
2024-06-27 17:36:55 +0100

    snipper bot page

---
+++ src/app/snippers/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SnipperBotPage() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <h1>SnipperBot Page</h1>
+    </div>
+  );
+}
+
+export default SnipperBotPage;
